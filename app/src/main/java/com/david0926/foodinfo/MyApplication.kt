fdd166c291804f46ecf194317f4fba1031862b3b
roian6 <roian6@naver.com>
2022-01-18 19:04:25 +0900

    UPDATE rename BaseApplication to MyApplication

--- app/src/main/java/com/david0926/foodinfo/MyApplication.kt
+++ app/src/main/java/com/david0926/foodinfo/MyApplication.kt
@@ -1,7 +1,7 @@
-package com.david0926.foodinfo.base
+package com.david0926.foodinfo
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class BaseApplication : Application()
+class MyApplication : Application()
