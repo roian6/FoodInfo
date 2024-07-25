1fed617404f26b1039ef69724cb439616ddec03d
roian6 <roian6@naver.com>
2024-07-25 21:25:30 +0900

    update sdk version

--- app/src/main/java/com/david0926/foodinfo/data/remote/FoodService.kt
+++ app/src/main/java/com/david0926/foodinfo/data/remote/FoodService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface FoodService {
-    @GET("/B553748/CertImgListService/getCertImgListService")
+    @GET("/B553748/CertImgListServiceV2/getCertImgListServiceV2")
     suspend fun getFoods(
         @Query("ServiceKey") ServiceKey: String?,
         @Query("prdlstReportNo") prdlstReportNo: String?,
